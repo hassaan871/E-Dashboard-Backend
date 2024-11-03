da473b64310fef748cc58d4e227ae80c15cb8562
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-11-03 18:59:03 +0500

    Login API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,31 @@
 const User = require('../models/User')
 
-const signupUser = async (req, res)=>{
-    try{
+const signupUser = async (req, res) => {
+    try {
         // app.use(express.json())
         let user = new User(req.body)
         let result = await user.save()
+        result = result.toObject()
+        delete result.password
         res.send(result)
-    } catch (err){
+    } catch (err) {
         res.status(500).send(err.message)
     };
 }
 
-module.exports = {signupUser}
+const loginUser = async (req, res) => {
+    try {
+        if (!req.body.email || !req.body.password) {
+            return res.send({ result: "Enter both Email and Password" })
+        }
+        let user = await User.findOne(req.body).select("-password")
+        if (!user) {
+            res.send({ result: "No user found" })
+        }
+        res.send(user)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+module.exports = { signupUser, loginUser }
