7233fba7abfb1cbfa790697327c659c3e97508f0
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-11-07 19:38:33 +0500

    product Route created

---
+++ routes/productRoute.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const { addProduct } = require('../controllers/productController')
+const router = express.Router()
+
+router.post('/add-product', addProduct)
+
+module.exports = router;
